24d5071786318228a02c0d948bcfbbe0cb8c2a6b
adr <senhoraroldo@gmail.com>
2021-05-05 09:40:49 -0300

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,17 @@
+// import vemBootstrap from './modules/botstrapimport.js';
+// import * as animacao from './modules/animation.js';
+// import * as carouselClick from './modules/carousel-change.js';
+// import * as config from './modules/swiper__props.js'; //Configs do Swiper
+import Dom from './modules/constructors.js'; //selecionar elementos
+import menu from './modules/menu.js'; //js do menu
+
+menu();
+
+
+// ★ PAGES ★ 
+const pageHome = new Dom().el("#page__home")
+
+if (pageHome) { // ★ HOME 
+    new Dom().bodyClass("body__home")
+
+}
