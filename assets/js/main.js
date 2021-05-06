3fa13bf1ac7fd100f5ca2d98429413a621d1af21
adr <senhoraroldo@gmail.com>
2021-05-06 12:28:11 -0300

    carousel home

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,12 +1,14 @@
 // import vemBootstrap from './modules/botstrapimport.js';
 // import * as animacao from './modules/animation.js';
 // import * as carouselClick from './modules/carousel-change.js';
-// import * as config from './modules/swiper__props.js'; //Configs do Swiper
+import Swiper from 'https://unpkg.com/swiper/swiper-bundle.esm.browser.min.js'
+import * as config from './modules/swiper__props.js'; //Configs do Swiper
 import Dom from './modules/constructors.js'; //selecionar elementos
 import menu from './modules/menu.js'; //js do menu
 
 menu();
 
+const mediaQuebra = window.matchMedia('(max-width: 1024px)').matches
 
 // ★ PAGES ★ 
 const pageHome = new Dom().el("#page__home")
@@ -14,7 +16,12 @@ const pageBlogSingle = document.querySelector('#page__blog-single');
 
 if (pageHome) { // ★ HOME 
     new Dom().bodyClass("body__home")
+    new Swiper(".swiper__container-depo", config.propsComent)
 
-} else if (pageBlogSingle) {
+    if (mediaQuebra)
+        new Swiper(".swiper__container-quebra", config.props2)
+
+
+} else if (pageBlogSingle) {// ★ Page BLOG SINGLE
     new Dom().bodyClass("body__blog-single");
 }
